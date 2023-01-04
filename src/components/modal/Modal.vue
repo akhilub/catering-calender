9d558140b929c549b6074a0d8c60acfd0b56084c
Mahak <mahak@mezeh.net>
2023-01-04 10:12:07 -0500

    added catering calendar and fixed tailwind styling

---
+++ src/components/modal/Modal.vue
@@ -0,0 +1,45 @@
+<template>
+    <Transition name="fade">
+      <div
+        v-if="showing"
+        @click.self="close"
+        class="
+          fixed z-50 inset-0 top-0 w-full h-screen flex items-center justify-center bg-semi-75"
+      >
+        <div class="relative w-full md:max-w-lg bg-white">
+          <slot />
+        </div>
+      </div>
+    </Transition>
+  </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  props: {
+    showing: {
+      required: true,
+      type: Boolean,
+    },
+    redirect: {
+      type: Boolean,
+    },
+  },
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+  },
+});
+</script>
+
+  <style scoped>
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: all 0.4s;
+  }
+  .fade-enter,
+  .fade-leave-to {
+    opacity: 0;
+  }
+  </style>
